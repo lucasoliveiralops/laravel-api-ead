149f82521c82b92d7cd59261f445a5897f08df7f
lucasoliveiralops <66143904+lucasoliveiralops@users.noreply.github.com>
2022-06-26 21:08:54 -0300

    First Commit

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::middleware('auth:sanctum')->get('/usesr', function (Request $request) {
+    return $request->user();
+});
