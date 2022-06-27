e266c11fca2d31bc835e8a1513dca1bcd7b6158e
lucasoliveiralops <66143904+lucasoliveiralops@users.noreply.github.com>
2022-06-26 21:14:35 -0300

    Configurando aplicação laravel para ser apenas uma api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/', function () {
+    return response()->json([
+        'sucess' => true,
+    ]);
+});
 
 Route::middleware('auth:sanctum')->get('/usesr', function (Request $request) {
     return $request->user();
